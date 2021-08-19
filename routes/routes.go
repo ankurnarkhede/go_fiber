975a654cea3bbdc737b26fac17ce82801901a2e9
RohitKuwar <rohit1kuwar@gmail.com>
2021-08-19 10:08:18 +0530

    added routes

---
+++ routes/routes.go
@@ -0,0 +1,16 @@
+package routes
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/RohitKuwar/go_fiber/controllers"
+)
+
+func Setup(app *fiber.App) {
+
+	app.Get("/goals", controllers.GetGoals)
+	app.Get("/goals/:id", controllers.GetGoal)
+	app.Post("/goals/add", controllers.CreateGoal)
+	app.Put("/goals/:id", controllers.UpdateGoal)
+	app.Delete("/goals/:id", controllers.DeleteGoal)
+
+}
