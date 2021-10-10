aef540b88bf0a7a898406bf93d59e75de5e1ad24
RohitKuwar <rohit1kuwar@gmail.com>
2021-10-10 14:32:12 +0530

    removed logger

--- main.go
+++ main.go
@@ -7,13 +7,13 @@ import (
 	// "github.com/joho/godotenv"
 	"github.com/RohitKuwar/go_fiber/routes"
 	"github.com/gofiber/fiber/v2"
-	"github.com/gofiber/fiber/v2/middleware/logger"
+	// "github.com/gofiber/fiber/v2/middleware/logger"
 )
 
 func main() {
 	app := fiber.New()
 	
-	app.Use(logger.New())
+	// app.Use(logger.New())
 	routes.Setup(app)
 
 	app.Listen(":8000")
